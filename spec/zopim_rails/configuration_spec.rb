5ee38e8ac5b4c96dea9d8376607ec77f6554835f
Wittawas Wisarnkanchana <unstartable@gmail.com>
2015-05-09 23:55:45 +0700

    add version spec

--- spec/zopim_rails/configuration_spec.rb
+++ spec/zopim_rails/configuration_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 require_relative '../../lib/zopim_rails/configuration'
+# require_relative '../../lib/zopim_rails/configuration'
 
 describe ZopimRails::Configuration do
 
@@ -13,4 +14,10 @@ describe ZopimRails::Configuration do
     expect(ZopimRails.configuration.api_key).to be_a_kind_of(String)
     expect(ZopimRails.configuration.api_key).to eq 'replace-me-with-your-api_key'
   end
+
+  it "should return version correctly" do
+    expect(ZopimRails.configuration.version).to be_a_kind_of(String)
+    expect(ZopimRails.configuration.version).to eq '0.0.6'
+  end
+
 end
---
+++ spec/zopim_rails/version_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+require_relative '../../lib/zopim_rails/version'
+
+describe ZopimRails do
+
+  it "should return version correctly" do
+    expect(ZopimRails::VERSION).to be_a_kind_of(String)
+    expect(ZopimRails::VERSION).to eq '0.0.6'
+  end
+end
