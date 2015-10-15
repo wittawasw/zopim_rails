0a32f18f6510e0dead50634359279ee437fd99ca
wittawasw💤 <start@wittawasw.com>
2015-10-15 19:40:29 +0700

    update spec

--- spec/zopim_rails/configuration_spec.rb
+++ spec/zopim_rails/configuration_spec.rb
@@ -15,9 +15,4 @@ describe ZopimRails::Configuration do
     expect(ZopimRails.configuration.api_key).to eq 'replace-me-with-your-api_key'
   end
 
-  it "should return version correctly" do
-    expect(ZopimRails.configuration.version).to be_a_kind_of(String)
-    expect(ZopimRails.configuration.version).to eq '0.0.6'
-  end
-
 end
